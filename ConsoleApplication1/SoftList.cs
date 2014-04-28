5c9f8a9934d2fffcc181090a438faf8f2bd95049
Eroatlan <florentburon@gmail.com>
2014-04-28 13:11:18 +0200

    Pushed shit

---
+++ ConsoleApplication1/SoftList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListingSoftware
+{
+    class SoftList : List<Software>
+    {
+        private List<Software> list;
+
+        //implementation of List constructor
+        public SoftList() : base()
+        {
+            
+        }
+    }
+}
